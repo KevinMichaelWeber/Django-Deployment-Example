71cc949f8fda01690aba76f60cce490f5e1b1025
Kevin Weber <kweb609@yahoo.com>
2022-10-12 16:13:29 -0400

    first commit

---
+++ basic_app/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from basic_app import views
+
+# urlpatterns = [
+#     path("",views.index),
+#     path("basicapp/",include("basic_app.urls")),
+#     path("admin/", admin.site.urls),
+
+# ]
